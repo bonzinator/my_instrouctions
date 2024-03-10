082d10767ae01d5ba141f38d95a6b07c0771200f
Ваше Имя <you@example.com>
2024-03-10 19:39:09 +0300

    modified:   Scripts/ssl-serts/generate_ssl.sh

--- Scripts/ssl-serts/generate_ssl.sh
+++ Scripts/ssl-serts/generate_ssl.sh
@@ -2,7 +2,7 @@
 
 # В секции CN и DNS указываем нужные имена домена.
 
-sudo openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 -keyout server.key -out server.crt -subj "/C=RU/ST=Moscow/L=Moscow/O=Rebrain/CN=example.ru" -addext "subjectAltName = DNS:example.ru"
+sudo openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 -keyout server.key -out server.crt -subj "/C=RU/ST=Moscow/L=Moscow/O=Rebrain/OU=Rebrain/CN=example.ru" -addext "subjectAltName = DNS:example.ru"
 cat server.crt server.key > server.pem
 
 # После этого файл server.pem можно использовать в nginx.conf или haproxy.cfg
